dfbca7997849aca9823dbfd2b622754255936193
Balveer Singh <balveersingh1731147@gmail.com>
2023-08-07 17:50:38 +0530

    some update

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ let content = document.getElementById("content");
 let author = document.getElementById("author");
 let btn = document.getElementById("new-quote");
 
-let url = "https://api.quotable.io/random";
+let url = "https://type.fit/api/quotes";
 
 async function fetchData(urlkey) {
   try {
+    let random = Math.floor(Math.random() * 16);
     let response = await fetch(urlkey);
     const result = await response.json();
-    content.innerHTML = `"${result.content}"`;
-    author.innerHTML = `~${result.author}`;
+    console.log(result[random]);
+    content.innerHTML = `"${result[random].text}"`;
+    author.innerHTML = `~${result[random].author}`;
   } catch (error) {
     console.log(error.message);
   }
